3eea65ede0766af8903ce0bff99366a046eb87ab
William Woodall <wjwwood@gmail.com>
2011-02-13 13:08:53 -0600

    Tested the SegwayStatus msg changes and did some more build file ignoring cleanup.

---
+++ segway_rmp/segway_rmp200/msg/lisp/segway_rmp200/_package.lisp
@@ -0,0 +1,7 @@
+(defpackage segway_rmp200-msg
+  (:use cl
+        roslisp-msg-protocol)
+  (:export
+   "<SEGWAYSTATUS>"
+  ))
+
--- segway_rmp/segway_rmp200/src/segway_rmp200.cpp
+++ segway_rmp/segway_rmp200/src/segway_rmp200.cpp
@@ -12,44 +12,37 @@
 std::string segway_name="segway";
 CSegwayRMP200 *segway;
 bool has_been_commanded = false;
-int segway_motor_timeout = 0.5;
+double segway_motor_timeout = 0.5;
 bool has_been_stopped = false;
+ros::NodeHandle *n;
 std::string frame_id = "base_link";
 ros::Publisher segway_status_pub;
-
-void cmd_velCallback(const geometry_msgs::Twist::ConstPtr& msg) {
-    has_been_commanded = true;
-    segway->move(msg->linear.x, msg->angular.z);
-    n.createTimer(ros::Duration(segway_motor_timeout), motor_timeoutCallback, true);
-}
 
 void statusCallback(const ros::TimerEvent& e) {
     segway_rmp200::SegwayStatus msg;
     
-    msg.header.time = ros::Time::now();
+    msg.header.stamp = ros::Time::now();
     msg.header.frame_id = frame_id;
     
     // Grab mutex for the status data
-    segway.access_status.enter();
-    msg.pitch_angle = segway.pitch_angle;
-    msg.pitch_rate = segway.pitch_rate;
-    msg.roll_angle = segway.roll_angle;
-    msg.roll_rate = segway.roll_rate;
-    msg.left_wheel_velocity = segway.left_wheel_velocity;
-    msg.right_wheel_velocity = segway.right_wheel_velocity;
-    msg.yaw_rate = segway.yaw_rate;
-    msg.servo_frames = segway.servo_frames;
-    msg.left_wheel_displacement = segway.left_wheel_displ;
-    msg.right_wheel_displacement = segway.right_wheel_displ;
-    msg.forward_displacement = segway.forward_displ;
-    msg.yaw_displacement = segway.yaw_displ;
-    msg.left_motor_torque = segway.left_torque;
-    msg.right_motor_torque = segway.right_torque;
-    msg.operation_mode = segway.mode;
-    msg.gain_schedule = segway.gain_schedule;
-    msg.ui_battery = segway.ui_battery;
-    msg.powerbase_battery = segway.powerbase_battery;
-    segway.access_status.exit();
+    msg.pitch_angle = segway->get_pitch_angle();
+    msg.pitch_rate = segway->get_pitch_rate();
+    msg.roll_angle = segway->get_roll_angle();
+    msg.roll_rate = segway->get_roll_rate();
+    msg.left_wheel_velocity = segway->get_left_wheel_velocity();
+    msg.right_wheel_velocity = segway->get_right_wheel_velocity();
+    msg.yaw_rate = segway->get_yaw_rate();
+    msg.servo_frames = segway->get_servo_frames();
+    msg.left_wheel_displacement = segway->get_left_wheel_displacement();
+    msg.right_wheel_displacement = segway->get_right_wheel_displacement();
+    msg.forward_displacement = segway->get_forward_displacement();
+    msg.yaw_displacement = segway->get_yaw_displacement();
+    msg.left_motor_torque = segway->get_left_motor_torque();
+    msg.right_motor_torque = segway->get_right_motor_torque();
+    msg.operation_mode = segway->get_operation_mode();
+    msg.gain_schedule = segway->get_gain_schedule();
+    msg.ui_battery = segway->get_ui_battery_voltage();
+    msg.powerbase_battery = segway->get_powerbase_battery_voltage();
     
     segway_status_pub.publish(msg);
 }
@@ -60,15 +53,21 @@ void motor_timeoutCallback(const ros::TimerEvent& e) {
         has_been_stopped = false;
     } else if(!has_been_stopped) { // If it hasn't been commanded since and it hasn't been stopped yet, stop it
         ROS_WARN("Motor timeout reached, stopping segway.");
-        has_ben_stopped = true;
+        has_been_stopped = true;
         segway->stop();
     }
+}
+
+void cmd_velCallback(const geometry_msgs::Twist::ConstPtr& msg) {
+    has_been_commanded = true;
+    segway->move(msg->linear.x, msg->angular.z);
+    n->createTimer(ros::Duration(segway_motor_timeout), motor_timeoutCallback, true);
 }
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "segway_rmp200");
 
-    ros::NodeHandle n;
+    n = new ros::NodeHandle;
 
     ROS_INFO("Setting up Segway Interface.");
 
@@ -99,23 +98,23 @@ int main(int argc, char **argv) {
         exit(-1);
     }
 
-    ros::Subscriber sub = n.subscribe("cmd_vel", 1000, cmd_velCallback);
+    ros::Subscriber sub = n->subscribe("cmd_vel", 1000, cmd_velCallback);
 
     ROS_INFO("Segway Ready.");
 
     // Setup Status Loop
     int segway_status_rate;
-    n.param("segway_status_rate", segway_status_rate, 2);
-    ros::Timer status_timer = n.createTimer(ros::Duration(1.0/segway_status_rate), statusCallback);
+    n->param("segway_status_rate", segway_status_rate, 2);
+    ros::Timer status_timer = n->createTimer(ros::Duration(1.0/segway_status_rate), statusCallback);
 
     // Setup Motor Timeout
-    n.param("segway_motor_timeout", segway_motor_timeout, 0.5);
+    n->param("segway_motor_timeout", segway_motor_timeout, 0.5);
     
     // Get frame id parameter
-    n.param("frame_id", frame_id, "base_link");
+    n->param("frame_id", frame_id, std::string("base_link"));
     
     // Setup the Segway Status Publisher
-    segway_status_pub = n.advertise<std_msgs::String>("segway_status", 1000);
+    segway_status_pub = n->advertise<segway_rmp200::SegwayStatus>("segway_status", 1000);
 
     ros::spin();
 
